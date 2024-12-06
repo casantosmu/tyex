203ef5f41214cf5db70848450fc1e7d25aae6513
Carlos Santos <casantosmu@gmail.com>
2024-12-06 07:54:00 +0100

    Basic Setup and Initialization

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,47 @@
-export function sum(a: number, b: number) {
-  return a + b;
+import express, { type Express } from "express";
+import Ajv from "ajv";
+import addFormats from "ajv-formats";
+
+interface Options {
+  ajv?: Ajv;
+}
+
+export default function texpress(app?: Express, options?: Options) {
+  if (!app) {
+    app = express();
+  }
+
+  let ajv = options?.ajv;
+  if (!ajv) {
+    ajv = addFormats(
+      new Ajv({
+        allErrors: true,
+        removeAdditional: "all",
+        useDefaults: true,
+        coerceTypes: "array",
+      }),
+      [
+        "date-time",
+        "time",
+        "date",
+        "email",
+        "hostname",
+        "ipv4",
+        "ipv6",
+        "uri",
+        "uri-reference",
+        "uuid",
+        "uri-template",
+        "json-pointer",
+        "relative-json-pointer",
+        "regex",
+        "binary",
+      ],
+    );
+  }
+
+  return {
+    express: app,
+    ajv,
+  };
 }
---
+++ tests/setup.test.ts
@@ -0,0 +1,34 @@
+import { describe, expect, test } from "vitest";
+import express from "express";
+import Ajv from "ajv";
+
+import texpress from "../src";
+
+describe("Basic Setup and Initialization", () => {
+  test("Should initialize with default Express app when no app is provided", () => {
+    const t = texpress();
+    expect(t.express).toBeInstanceOf(Function);
+    expect(t.express.listen.bind(t.express)).toBeDefined();
+  });
+
+  test("Should initialize with custom Express app when provided", () => {
+    const app = express();
+    const t = texpress(app);
+    expect(t.express).toBe(app);
+  });
+
+  test("Should initialize with default AJV instance when no options provided", () => {
+    const t = texpress();
+    expect(t.ajv).toBeInstanceOf(Ajv);
+    expect(t.ajv.opts.allErrors).toBe(true);
+    expect(t.ajv.opts.removeAdditional).toBe("all");
+    expect(t.ajv.opts.useDefaults).toBe(true);
+    expect(t.ajv.opts.coerceTypes).toBe("array");
+  });
+
+  test("Should initialize with custom AJV instance when provided in options", () => {
+    const customAjv = new Ajv({ strict: true });
+    const t = texpress(undefined, { ajv: customAjv });
+    expect(t.ajv).toBe(customAjv);
+  });
+});
