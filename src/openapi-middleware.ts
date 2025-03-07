1210c61755b1c9293bbb86a7f0f7758a53036bc6
Carlos Santos <casantosmu@gmail.com>
2025-03-07 22:08:36 +0100

    fix: export Options interface from openapi-middleware

--- src/openapi-middleware.ts
+++ src/openapi-middleware.ts
@@ -2,7 +2,7 @@ import type { RequestHandler } from "express-serve-static-core";
 import type { OpenAPIV3 } from "openapi-types";
 import { oasGenerator } from "./oas-generator";
 
-interface Options {
+export interface Options {
   document?: Omit<OpenAPIV3.Document, "paths">;
 }
 
